886237e26b5c6a2b24da748774bcf7881e6372ad
fabrice8 <fabricciochumpitazi@gmail.com>
2026-02-17 11:02:34 +0100

    Ajout de la simulation de données

--- Scripts_Arduino/Cpp_NB-IoT/Cpp_NB-IoT.ino
+++ Scripts_Arduino/Cpp_NB-IoT/Cpp_NB-IoT.ino
@@ -1,63 +1,14 @@
-#include <MKRNB.h>
-#include <ArduinoMqttClient.h>
-#include <Wire.h>
-#include <Adafruit_BME280.h>
-#include <Arduino_JSON.h>
-
-// --- CONFIGURATION ---
-const char broker[] = "167.99.87.179"; // <- IP
-int        port     = 1883;
-const char topic[]  = "composteur/capteur";
-
-// Paramètres de la SIM (souvent vide pour le NB-IoT/LTE-M)
-const char pin[]      = ""; 
-const char apn[]      = ""; // Laisse vide ou mets l'APN de ton opérateur si besoin
-
-// --- OBJETS ---
-NB nbAccess;
-GPRS gprs;
-NBClient client;
-MqttClient mqttClient(client);
-Adafruit_BME280 bme;
-
-void setup() {
-  Serial.begin(9600);
-  while (!Serial);
-
-  // Initialisation BME280
-  if (!bme.begin(0x76)) {
-    Serial.println("BME280 introuvable !");
-    while (1);
-  }
-
-  // Connexion au réseau cellulaire
-  Serial.println("Connexion au réseau NB-IoT...");
-  while (nbAccess.begin(pin, apn) != NB_READY || gprs.attachGPRS() != GPRS_READY) {
-    Serial.print(".");
-    delay(1000);
-  }
-  Serial.println("\nConnecté au réseau !");
-
-  // Connexion au Broker MQTT sur ta VM
-  Serial.print("Connexion au broker MQTT...");
-  if (!mqttClient.connect(broker, port)) {
-    Serial.print("Échec MQTT code: ");
-    Serial.println(mqttClient.connectError());
-    while (1);
-  }
-  Serial.println("Connecté au Cloud !");
-}
-
 void loop() {
   mqttClient.poll();
 
-  // On lit les valeurs
-  float t = bme.readTemperature();
-  float h = bme.readHumidity();
-  float p = bme.readPressure() / 100.0F;
+  // --- SIMULATION DES DONNÉES ---
+  // On génère des valeurs réalistes pour un composteur
+  // random(min, max) génère un entier, on divise pour avoir des décimales
+  float t = random(2000, 4500) / 100.0; // Simule entre 20.00 et 45.00 °C
+  float h = random(6000, 8500) / 100.0; // Simule entre 60.00 et 85.00 % d'humidité
+  float p = random(101000, 101500) / 100.0; // Simule autour de 1013 hPa
 
-  // On crée le message JSON manuellement pour maîtriser les virgules
-  // String(valeur, 2) force exactement 2 chiffres après la virgule
+  // On crée le message JSON
   String jsonPayload = "{";
   jsonPayload += "\"temperature\":" + String(t, 2) + ",";
   jsonPayload += "\"humidite\":" + String(h, 2) + ",";
@@ -65,7 +16,7 @@ void loop() {
   jsonPayload += "}";
 
   // Publication
-  Serial.print("Envoi des données : ");
+  Serial.print("Envoi des données SIMULÉES : ");
   Serial.println(jsonPayload);
   
   mqttClient.beginMessage(topic);
